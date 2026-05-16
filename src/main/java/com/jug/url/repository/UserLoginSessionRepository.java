d4b1a399a43d4b14701dbdacade51a7700f3a013
Abdulroqib-Adeleke <adelekeabdulroqib29@gmail.com>
2026-05-16 01:19:23 +0100

    Added user login session repository

---
+++ src/main/java/com/jug/url/repository/UserLoginSessionRepository.java
@@ -0,0 +1,13 @@
+package com.jug.url.repository;
+
+import com.jug.url.model.UserLoginSession;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+import java.util.UUID;
+
+public interface UserLoginSessionRepository extends JpaRepository<UserLoginSession, UUID> {
+
+    Optional<UserLoginSession> findByUserId(UUID userId);
+
+}
