dfb8e05127559b6a6894c74fa26f7ce813614a81
Abdulroqib-Adeleke <adelekeabdulroqib29@gmail.com>
2026-05-19 14:27:04 +0100

    Populated user proxy class

--- src/main/java/com/jug/url/dto/proxy/UserProxy.java
+++ src/main/java/com/jug/url/dto/proxy/UserProxy.java
@@ -1,4 +1,34 @@
 package com.jug.url.dto.proxy;
 
+import com.jug.url.enums.Roles;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import java.time.LocalDateTime;
+import java.util.Set;
+import java.util.UUID;
+
+@Data
+@NoArgsConstructor
 public class UserProxy {
+    private UUID id;
+    private String name;
+    private String email;
+    private String password;
+    private Set<Roles> roles;
+    LocalDateTime createdDate;
+    LocalDateTime updatedDate;
+
+
+
+    public UserProxy(UUID id, String name,
+                     String password, String email,
+                     LocalDateTime createdDate,
+                     LocalDateTime updatedDate) {
+        this.id = id;
+        this.name = name;
+        this.password = password;
+        this.email = email;
+        this.createdDate = createdDate;
+        this.updatedDate = updatedDate;
+    }
 }
